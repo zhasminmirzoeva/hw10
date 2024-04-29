60dfae3748d7e174268e8da34e5c001c965c361d
zhasminmirzoeva <114018963+zhasminmirzoeva@users.noreply.github.com>
2024-04-29 10:03:45 +0300

    ДЗ 10

---
+++ hw10/client2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char message[1024];
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        read(sock, message, 1024);
+        printf("Client 1: %s\n", message);
+
+        if (strcmp(message, "The End\n") == 0) {
+            break;
+        }
+
+        printf("Enter message for Client 1: ");
+        fgets(message, 1024, stdin);
+        send(sock, message, strlen(message), 0);
+    }
+
+    close(sock);
+
+    return 0;
+}
---
+++ hw10/server.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        read(new_socket, buffer, 1024);
+        printf("Client 1: %s\n", buffer);
+
+        if (strcmp(buffer, "The End\n") == 0) {
+            break;
+        }
+
+        send(new_socket, buffer, strlen(buffer), 0);
+    }
+
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
